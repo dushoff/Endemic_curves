eb97916dae0fbd81090b240f0ef5d68ca9c9e6b5
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-06-03 16:46:53 -0400

    Add a simpler propCurve for AIMS

--- propCurves.R
+++ propCurves.R
@@ -7,8 +7,6 @@ clist <- lapply(cvlist, function(cv){
 	I <- propI(g, mult=bet)
 })
 
-# R0 version
-
 cx <- 1.5
 lwd <- 2.5
 par(mar = c(5, 6, 4, 1.5), cex.lab = cx, cex.axis = cx, cex = cx)
@@ -62,3 +60,16 @@ for (i in length(clist):1){
 legend('topleft',leg =cvlist, lty = 1, col = cols, bty = 'n', lwd = lwd,
        title = 'coefficient of variation')
 abline(h=0.1, lty=2, lwd = lwd)
+
+## Even simpler plot (for a public lecture)
+plot(
+	bet*(1+cvlist[[1]]^2), clist[[1]],
+	type="l", log="x", lwd = lwd,
+	xlab="Population-level risk", ylab="Proportion affected", main = '',
+	xlim=c(0.1, max(bet)), bty='n', ylim = c(0,1.05), 
+	yaxt='n')
+axis(2, seq(0,1, by = .25), NA)
+axis(2, seq(0,1, by = .5), las = 2)
+## abline(h=0.1, lty=2, lwd = lwd)
+## legend('topleft', c('homogeneous'), lwd = 2, bty = 'n')
+       
