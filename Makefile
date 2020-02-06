58559e2dd5c557adbd55bef3a1d348f47459b140
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-02-05 23:09:16 -0500

    Autocommit (Endemic_curves)

--- Makefile
+++ Makefile
@@ -22,6 +22,9 @@ groupTest.Rout simpleGroup.Rout: distFuns.Rout groupFuns.Rout
 
 propCurves.Rout: distFuns.Rout groupFuns.Rout propFuns.Rout propCurves.R
 
+## A version of the first propCurves with a higher hline for talks
+aedes.Rout: distFuns.Rout groupFuns.Rout propFuns.Rout aedes.R
+
 ######################################################################
 
 -include $(ms)/git.mk
---
+++ aedes.R
@@ -0,0 +1,22 @@
+bet <- exp(seq(log(0.1), log(8), length.out=100))
+cvlist <- seq(0, 2, by=0.5)
+numGroups <- 20
+
+clist <- lapply(cvlist, function(cv){
+	g <- makeDist(CV=cv, groups=numGroups)
+	I <- propI(g, mult=bet)
+})
+
+cx <- 1.5
+lwd <- 2.5
+par(mar = c(5, 6, 4, 1.5), cex.lab = cx, cex.axis = cx, cex = cx)
+## simple plot for comparison
+plot(
+	bet*(1+cvlist[[1]]^2), clist[[1]],
+	type="l", log="x", lwd = lwd,
+	xlab=expression(R[0]), ylab="Proportion affected", main = 'endemic equilibrium',
+	xlim=c(0.1, max(bet)), bty='n', ylim = c(0,1.05), 
+	yaxt='n')
+axis(2, seq(0,1, by = .25), NA)
+axis(2, seq(0,1, by = .5), las = 2)
+abline(h=0.5, lty=2, lwd = lwd)
